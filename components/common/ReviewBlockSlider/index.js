71f3c367ffe3f839fd8df29816d044e495d5eed0
Sergey Andreev <aandreev.sm@gmail.com>
2018-12-09 21:35:23 +0300

    Fixed error

--- components/common/ReviewBlockSlider/index.js
+++ components/common/ReviewBlockSlider/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Slider from "react-slick";
-import "./index.scss"
-import styles from 'styled-components'
+import Slider from 'react-slick';
+import styles from 'styled-components';
+import "./index.scss";
 
 const BlockSliderReviewsItem = styles.div`
   padding: .38em;
 `; 
 
-const Review = styles.p`
+const ReviewBlock = styles.p`
   font-size: .5em;
   color: #4a4a4a;
   line-height: 1.5;
@@ -61,24 +61,19 @@ const ReviewsJob = styles.span`
   }
 `;
 
-
-
-
 const BlockSliderSlideReview = styles.div`
   max-width: 100% !important;
 `;
-
 
 const BlockSliderReview = styles.section `
   max-width: 100%;
 `;
 
-
 const Review = ({ faceImg, reviewText, bottom, name, prof }) => (
   <BlockSliderReviewsItem className="block-slider__reviews-item">
-    <Review className={`review ${bottom ? 'review-bottom': ''}`}>
+    <ReviewBlock className={`review ${bottom ? 'review-bottom': ''}`}>
         {reviewText}
-    </Review>
+    </ReviewBlock>
     <ReviwsAuthor className="reviews-author">
         <Img src={faceImg} alt={"Студент " + name} />
         <ReviewsAuthorData className="reviews-author__data">
@@ -90,96 +85,89 @@ const Review = ({ faceImg, reviewText, bottom, name, prof }) => (
 
 )
 
-
 const Block = (props)=> {
-  
-        const { client, manager } = props;
-        return (
-                <BlockSliderSlideReview className="block-slider__slide review">
-                        <div className="block-slider__reviews-block">
-                            <Review faceImg={client.avatar}
-                                    reviewText={client.text}
-                                    name={client.name}
-                                    prof={client.prof}
-                                    bottom={false}/>
-                            <Review faceImg={manager.avatar}
-                                    reviewText={manager.text}
-                                    name={manager.name}
-                                    prof={manager.prof}
-                                    bottom={true}/>     
-                        </div>
-                </BlockSliderSlideReview>
-            )
-    
+  const { client, manager } = props;
+  return (
+    <BlockSliderSlideReview className="block-slider__slide review">
+      <div className="block-slider__reviews-block">
+        <Review faceImg={client.avatar}
+          reviewText={client.text}
+          name={client.name}
+          prof={client.prof}
+          bottom={false}
+        />
+        <Review faceImg={manager.avatar}
+          reviewText={manager.text}
+          name={manager.name}
+          prof={manager.prof}
+          bottom={true}
+        />
+      </div>
+    </BlockSliderSlideReview>
+  )
 }
-
 
 const renderBlocks = (reviewBlockConfig) =>{
-
-    return reviewBlockConfig.map((item, index)=>(
-            <Block  
-                    key={index}
-                    client={item.client}
-                    manager={item.manager}
-                    blocksCount={reviewBlockConfig.length} />
-    ))
+  return reviewBlockConfig.map((item, index)=>(
+    <Block  
+      key={index}
+      client={item.client}
+      manager={item.manager}
+      blocksCount={reviewBlockConfig.length}
+    />
+  ))
 }
 
-
 class ReviewSlider extends Component{
-   
+  render(){
+      const { reviewBlockConfig } = this.props;
+      const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 660,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 1300,
+            settings: {
+              slidesToShow: 3,
+            }
+          }
+        ]
+      };
 
-    render(){
-        const { reviewBlockConfig } = this.props;
-        const settings = {
-            dots: false,
-            infinite: true,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            pauseOnHover: true,
-            arrows: true,
-            responsive: [
-                {
-                    breakpoint: 660,
-                    settings: {
-                        slidesToShow: 1,
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 3,
-                    }
-                },
-                {
-                    breakpoint: 992,
-                    settings: {
-                        slidesToShow: 3,
-                    }
-                },
-                {
-                    breakpoint: 1300,
-                    settings: {
-                        slidesToShow: 3,
-                    }
-                },
-
-            ]
-        };
-        
-        return (
-          <BlockSliderReview className="block-slider review">
-              <div className="slick">
-                      <Slider {...settings}> 
-                          {renderBlocks(reviewBlockConfig)}
-                      </Slider>
-              </div>
-          </BlockSliderReview>
-        )
-    }
+      return (
+        <BlockSliderReview className="block-slider review">
+          <div className="slick">
+            <Slider {...settings}> 
+              {renderBlocks(reviewBlockConfig)}
+            </Slider>
+          </div>
+        </BlockSliderReview>
+      )
+  }
 }
 
 export default ReviewSlider;
-
