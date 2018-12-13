0f98b6a8d2e65fca84ed60b418f354aa97c3e4f6
Shalser <shalser82@yandex.ru>
2018-12-13 20:33:22 +0300

    remove h2 tag

--- utils/redirect.js
+++ utils/redirect.js
@@ -1,4 +1,10 @@
 module.exports = {
     '/abc': '/def',
     '/ololo': '/ajaja',
+    'https://besmarter.ru/diplomnaya-rabota-po-buhgalterskomu-uchetu': 'https://besmarter.ru/diplom-na-zakaz/buhgalterskij-uchet',
+    '//diplomnaya-rabota-po-menedjmentu': '//besmarter.ru/diplom-na-zakaz/menedjment',
+    '//diplomnaya-rabota-po-pedagogike':'/diplom-na-zakaz/pedagogika',
+    '/diplomnaya-rabota-po-yurisprydencii': '/diplom-na-zakaz/yurisprudenciya-pravo',
+    '/onas': 'https://besmarter.ru/about',
+    '/sitemap': '/besmarter.ru/map',
 };
