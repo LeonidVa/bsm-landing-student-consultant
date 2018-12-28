590132e0fac8dadc16e740dd44555e956f1404a5
Shalser <shalser82@yandex.ru>
2018-12-28 21:38:30 +0300

    block timer

--- components/content/offers/20uz.js
+++ components/content/offers/20uz.js
@@ -2,7 +2,7 @@ import FormWithTimer from 'components/common/forms/WithTimer'
 
 const Offer20uz = () => (
     <section className="block-form-timer" style={{backgroundImage: "url(" + require('static/images/block/h.jpg') + ")"}}>
-        <span className="block-form-timer__title__h2">
+        <span  className="block-form-timer__title__h2" style={{lineHeight: "initial"}}>
             <span style={{fontSize: "2em", marginTop: "-2em"}}>20%</span><br/>
             Успей получить<br/> скидку на пакет <br/> «Успешная Защита»
         </span>
