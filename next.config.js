1e844fc3a3dfbdac6d5bf7e0e3aa268770082252
Sergey Andreev <aandreev.sm@gmail.com>
2019-01-09 18:37:14 +0300

    Added RECAPTCHA ENV

--- next.config.js
+++ next.config.js
@@ -44,6 +44,7 @@ const nextConfig = {
       yaID: 132186,
       fbpID: '196710264445109',
       source: process.env.SOURCE,
+      recaptcha: process.env.RECAPTCHA
     },
   },
   ///assetPrefix: process.env.NODE_ENV === "production" ? 'https://cdn.cloudflare.com' : null,
