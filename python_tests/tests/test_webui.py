ef4209a3f77651e96066ae08b52951e1a5b1146e
Parviz Khavari <csctclan@gmail.com>
2019-01-12 12:27:22 +0300

    Added tests for Request Form and all Pages

---
+++ python_tests/tests/test_webui.py
@@ -0,0 +1,104 @@
+import json
+import logging
+import tempfile
+
+import allure
+import pytest
+
+
+@allure.story("Форма заказа")
+class TestForm:
+
+    @pytest.mark.webui
+    @pytest.mark.parametrize('form_id', [0, 1])
+    @allure.title("Проверка наличия формы и ее базового состояния")
+    def test_verify_form_present(self, web, form_id):
+        web.open()
+        forms = web.get_forms()
+        assert 2 == len(forms)
+        form = web.form(forms[form_id])
+        assert form.form_title().text
+        assert form.name_field().visible
+        assert form.phone_field().visible
+        assert form.more_info().visible
+        assert not form.email_field().visible
+        assert not form.theme_field().visible
+        assert not form.worktype_field().visible
+        assert not form.discipline_field().visible
+        assert not form.deadline_field().visible
+        assert not form.size_field().visible
+        assert not form.comment_field().visible
+        assert "Показать все поля▾" == form.more_info().text
+        assert form.recaptcha()
+        assert ("Отправляя эти данные, я принимаю Политику "
+                "конфиденциальности") == form.agreement().text
+        assert form.submit_button().visible
+
+    @pytest.mark.webui
+    @pytest.mark.parametrize('form_id', [0, 1])
+    @allure.title("Отправка формы с заполненными только обязательными полями")
+    def test_submit_minimal_form(self, web, form_id):
+        web.open()
+        forms = web.get_forms()
+        form = web.form(forms[form_id])
+        form.submit_minimal_form(phone="+79012345678")
+        assert form.modal_title().visible
+        assert "Спасибо!" == form.modal_title().text
+        assert form.modal_text().visible
+        assert ("Мы получили Ваше сообщение и скоро свяжемся "
+                "с Вами!") == form.modal_text().text
+        assert form.modal_image().visible
+        assert form.modal_close().visible
+        form.close_modal()
+        assert not form.modal_text().visible
+
+    @pytest.mark.webui
+    @pytest.mark.parametrize('form_id', [0, 1])
+    @allure.title("Отправка формы с всеми заполненными полями")
+    def test_submit_full_form(self, web, form_id, faker):
+        web.open()
+        forms = web.get_forms()
+        form = web.form(forms[form_id])
+        temp = tempfile.NamedTemporaryFile(delete=False)
+        temp.close()
+        form.submit_full_form(
+            phone="+79012345678", email=faker.email(), theme=faker.word(),
+            worktype=1, discipline=faker.job(), deadline="10.12.2030",
+            size=faker.pyint(), comment=faker.sentence(), file_path=temp.name
+        )
+        assert form.modal_title().visible
+        assert "Спасибо!" == form.modal_title().text
+        assert form.modal_text().visible
+        assert ("Мы получили Ваше сообщение и скоро свяжемся "
+                "с Вами!") == form.modal_text().text
+        assert form.modal_image().visible
+        assert form.modal_close().visible
+        form.close_modal()
+        assert not form.modal_text().visible
+
+
+@allure.story("Проверка доступности страниц")
+class TestPages:
+
+    @pytest.mark.webui
+    @allure.title("Проверка открытия главной страницы")
+    def test_verify_main_page(self, web):
+        web.open()
+        logging.info("Checking main page")
+        assert web.page_is_ok(True), "Main page is empty"
+        assert not web.status_404(), "Main page status 404"
+
+    @pytest.mark.webui
+    @allure.title("Проверка всех доступных страниц из sitemap")
+    def test_verify_all_pages(self, web):
+        links = web.dump_sitemap()
+        results = []
+        for link in links:
+            web.open(link, use_host=False)
+            logging.info(f"Checking page {link}")
+            is_ok = web.page_is_ok()
+            is_404 = web.status_404()
+            results.append(dict(link=link, ok=is_ok, ko=is_404))
+        bad_results = [i for i in results if not i['ok'] or i['ko']]
+        assert not bad_results, (f"Some of pages are not OK, take a "
+                                 f"look: {json.dumps(bad_results, indent=4)}")
---
+++ python_tests/web/elements/request_form.py
@@ -0,0 +1,123 @@
+from time import sleep
+
+import allure
+
+from web.web_element import WebDriverElement
+
+
+class RequestForm:
+
+    def __init__(self, form: WebDriverElement):
+        self.form = form
+
+    def modal_title(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__message .block-form__title")
+
+    def modal_text(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__message p")
+
+    def modal_image(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__message img")
+
+    def modal_close(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__message .modal__close")
+
+    @allure.step("Ждем появления модального окна")
+    def wait_modal_is_visible(self, timeout=10):
+        is_visible = False
+        for _ in range(timeout):
+            is_visible = self.modal_title().visible
+            if is_visible:
+                break
+            sleep(1)
+        return is_visible
+
+    @allure.step("Закрываем модальное окно")
+    def close_modal(self):
+        self.modal_close().scroll_with_offset(-80)
+        self.modal_close().click()
+
+    def form_title(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__form .block-form__title")
+
+    def name_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//input[@name=\"name\"]")
+
+    def phone_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//input[@name=\"phone\"]")
+
+    def email_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//input[@name=\"email\"]")
+
+    def theme_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//input[@name=\"theme\"]")
+
+    def worktype_field(self) -> WebDriverElement:
+        return self.form.find_by_css(".Dropdown-root")
+
+    def worktype_options(self) -> WebDriverElement:
+        return self.form.find_by_css(".Dropdown-option")
+
+    def discipline_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//input[@name=\"discipline\"]")
+
+    def deadline_field(self) -> WebDriverElement:
+        return self.form.find_by_css(".DayPickerInput input")
+
+    def size_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//input[@name=\"size\"]")
+
+    def comment_field(self) -> WebDriverElement:
+        return self.form.find_by_xpath(".//textarea[@id=\"comment\"]")
+
+    def file_field(self) -> WebDriverElement:
+        return self.form.find_by_css(".dropzone input")
+
+    def more_info(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__more-info")
+
+    def recaptcha(self) -> WebDriverElement:
+        return self.form.find_by_css("#captcha")
+
+    def agreement(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__agree")
+
+    def submit_button(self) -> WebDriverElement:
+        return self.form.find_by_css(".block-form__btn")
+
+    def click_submit(self):
+        self.submit_button().click()
+
+    def select_worktype(self, _id):
+        if _id is not None:
+            self.worktype_field().click()
+            self.worktype_options()[_id].click()
+
+    @allure.step("Заполняем форму только обязательными полями")
+    def submit_minimal_form(self, name="TEST-AUTO", phone=None):
+        self.name_field().fill(name)
+        self.phone_field().fill(phone)
+        self.recaptcha().click()
+        self.click_submit()
+        self.wait_modal_is_visible()
+
+    @allure.step("Заполняем всю форму")
+    def submit_full_form(
+            self, name="TEST-AUTO", phone="", email="", theme="",
+            worktype=None, discipline="", deadline="", size="", comment="",
+            file_path=""
+    ):
+        self.name_field().fill(name)
+        self.phone_field().fill(phone)
+        self.more_info().click()
+        self.email_field().fill(email)
+        self.theme_field().fill(theme)
+        self.select_worktype(worktype)
+        self.discipline_field().fill(discipline)
+        self.deadline_field().fill(deadline)
+        self.size_field().fill(size)
+        self.comment_field().fill(comment)
+        self.file_field().fill(file_path)
+        self.recaptcha().click()
+        self.click_submit()
+        self.wait_modal_is_visible()
