6e5494150db81a51a82eee8f678daba0c75fd1c5
Sergey Andreev <aandreev.sm@gmail.com>
2019-01-15 21:24:27 +0300

    Added form to landings

--- websites/student-consult/pages/index.js
+++ websites/student-consult/pages/index.js
@@ -256,19 +256,40 @@ class page extends Component {
             },
           ]}/>
 
-              <ServiceBlock
-                  color="#333333"
-                  url="#contacts"
-                  image={require('static/images/block/7.jpg')}
-                  gradient="rgba(199, 212, 229, 0.95), rgba(199, 212, 229, 0.7)"
-                  title="Консультации по любым типам работ!"
-                  text={<div>
-                      <p>От поступления до защиты</p>
-                      <p>От 3 000 рублей</p>
-                      <p>Срочно от 2 часов</p>
+          <ServiceBlock
+            color="#333333"
+            url="#contacts"
+            image={require('static/images/block/7.jpg')}
+            gradient="rgba(199, 212, 229, 0.95), rgba(199, 212, 229, 0.7)"
+            title="Консультации по любым типам работ!"
+            text={
+              <div>
+                <p>От поступления до защиты</p>
+                <p>От 3 000 рублей</p>
+                <p>Срочно от 2 часов</p>
+              </div>
+            }
+          />
 
-                  </div>}
-              />
+          <Form
+            isFull
+            formType="Получить доступ к услугам"
+            targetID="order"
+            title="Заказать помощь"
+            redForm={true}
+            buttonLabel="Отправить"
+            fields={[
+              { id: 1, label: 'Имя', name: 'name', type: 'text', placeholder: 'Ваше имя', required: true },
+              { id: 2, label: 'Телефон', name: 'phone', type: 'text', placeholder: 'Ваш телефон', required: true },
+              { id: 3, label: 'Почта', name: 'email', type: 'email', placeholder: 'email', required: false },
+              { id: 4, label: 'Тема', name: 'theme', type: 'text', placeholder: 'Тема работы', required: false },
+              { id: 5, label: 'Вид работы', name: 'worktype', type: 'text', placeholder: 'Укажите тип', required: false },
+              { id: 6, label: 'Предмет', name: 'discipline', type: 'text', placeholder: 'Предмет или дисциплина', required: false },
+              { id: 7, label: 'Срок', name: 'deadline', type: 'date', placeholder: 'Укажите срок', required: false },
+              { id: 9, label: 'Комментарии', name: 'comment', type: 'textarea', placeholder: 'Ваши комментарии', required: false },
+              { id: 10, label: 'Прикрепить Файл', name: 'file', type: 'file', placeholder: 'Добавить файл', required: false }
+            ]}
+          />
 
               <TextBlock>
                   <h2 className="title">
