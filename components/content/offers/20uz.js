49921c1482898cb851df4428f2bbd39e14fc2db0
Pavel <pasha.gubin232@gmail.com>
2019-01-17 16:49:56 +0200

    CurrentOffer block style fix

--- components/content/offers/20uz.js
+++ components/content/offers/20uz.js
@@ -4,14 +4,14 @@ const Offer20uz = () => (
     <section className="block-form-timer" style={{backgroundImage: "url(" + require('static/images/block/h.jpg') + ")"}}>
         <span  className="block-form-timer__title__h2" style={{lineHeight: "initial"}}>
             <span style={{fontSize: "2em", marginTop: "-2em"}}>20%</span><br/>
-            Успей получить<br/> скидку на пакет <br/> «Успешная Защита»
+            Успей получить скидку<br/> на пакет «Успешная Защита»
         </span>
         <FormWithTimer
             timerDuration={155555}
             formType="Скидка 20% на материалы"
             targetID="sale_20%"
         >
-            <p style={{fontSize: '70%', marginBottom: '11px'}}>при одновременном заказе вместе с работой</p>
+            <p style={{fontSize: '70%', margin: '15px 0'}}>при одновременном заказе вместе с работой</p>
         </FormWithTimer>
     </section>
 );
