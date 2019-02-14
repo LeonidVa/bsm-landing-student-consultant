61389693d4f5c4216390089897acb80ac592b5e6
Dmitry Rog <dmitry@Dmitrys-MacBook-Pro.local>
2019-02-14 16:58:38 +0400

    issue-84

--- data/form/main.js
+++ data/form/main.js
@@ -48,33 +48,33 @@ const workTypeOptions = [
 
 export default [
   {
-    id: 1, label: 'Имя', name: 'name', type: 'text', placeholder: 'Ваше имя', required: true,
+    id: 1, label: 'Имя', name: 'name', type: 'text', required: true,
   },
   {
-    id: 2, label: 'Телефон', name: 'phone', type: 'text', placeholder: 'Ваш телефон', required: true,
+    id: 2, label: 'Телефон', name: 'phone', type: 'text', required: true,
   },
   {
-    id: 3, label: 'Почта', name: 'email', type: 'email', placeholder: 'email', required: false,
+    id: 3, label: 'Почта', name: 'email', type: 'email', required: false,
   },
   {
-    id: 4, label: 'Тема', name: 'theme', type: 'text', placeholder: 'Тема работы', required: false,
+    id: 4, label: 'Тема', name: 'theme', type: 'text', required: false,
+  },
+  // {
+  //   id: 5, label: 'Вид работы', name: 'worktype', type: 'dropdown', options: workTypeOptions, placeholder: 'Укажите подходящий тип работы', required: false,
+  // },
+  {
+    id: 6, label: 'Предмет', name: 'discipline', type: 'text', required: false,
   },
   {
-    id: 5, label: 'Вид работы', name: 'worktype', type: 'dropdown', options: workTypeOptions, placeholder: 'Укажите подходящий тип работы', required: false,
+    id: 7, label: 'Срок сдачи', name: 'deadline', type: 'date', required: false,
   },
   {
-    id: 6, label: 'Предмет', name: 'discipline', type: 'text', placeholder: 'Предмет или дисциплина', required: false,
+    id: 8, label: 'Объём', name: 'size', type: 'text', required: false,
   },
   {
-    id: 7, label: 'Срок сдачи', name: 'deadline', type: 'date', placeholder: 'Укажите срок сдачи работы', required: false,
+    id: 9, label: 'Комментарии', name: 'comment', type: 'textarea', required: false,
   },
   {
-    id: 8, label: 'Объём', name: 'size', type: 'text', placeholder: 'Требуемый объём', required: false,
-  },
-  {
-    id: 9, label: 'Комментарии', name: 'comment', type: 'textarea', placeholder: 'Ваши комментарии', required: false,
-  },
-  {
-    id: 10, label: 'Прикрепить Файл', name: 'file', type: 'file', placeholder: 'Добавить файл', required: false,
+    id: 10, label: 'Прикрепить Файл', name: 'file', type: 'file', required: false,
   },
 ];
