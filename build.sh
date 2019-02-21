39d80d9725f21a97818b586613be2089d65a636c
Sergey D. Ivanov <me@ganzal.pro>
2019-02-21 17:31:19 +0300

    Updating development scripts

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ export NODE_ENV="production"
 set -e
 DIR="$( dirname $( realpath "${0}" ) )"
 chmod +x "${DIR}/dev.sh"
-chmod +x "${DIR}/pages.sh"
 chmod +x "${DIR}/start.sh"
 chmod +x "${DIR}/stop.sh"
-"${DIR}/pages.sh"
 yarn run build
--- dev.sh
+++ dev.sh
@@ -2,5 +2,19 @@
 export NODE_ENV="development"
 set -e
 DIR="$( dirname $( realpath "${0}" ) )"
-${DIR}/pages.sh
+
+if [ ! -e "${DIR}/bsm-shared" ]; then
+    if [ ! -d ${DIR}/../bsm-common ]; then
+        cd ../
+        git clone git@gitlab.com:besmarter/bsm-common.git
+        cd ${DIR}
+    fi
+
+    ln -s ../bsm-common/bsm-shared ./bsm-shared
+fi
+
+if [ ! -e "${DIR}/node_modules" ]; then
+    yarn install
+fi
+
 yarn run dev
